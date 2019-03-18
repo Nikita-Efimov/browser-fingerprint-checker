fd21714a4ab34bf2a94932cbf925925af81a6144
nikita-efimov <nikikot@list.ru>
2019-03-18 23:52:08 +0300

    added browser ver and os

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ var user = detect.parse(navigator.userAgent);
   // user.browser.version
   // user.os.name
 
-document.getElementById('user-agent-cont').innerHTML = user.browser.family + ' ' + user.browser.version + ' ' + user.os.name
+document.getElementById('user-agent-browser').innerHTML = user.browser.family + ' ' + user.browser.version
+document.getElementById('user-agent-os').innerHTML = user.os.name
