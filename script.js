a72c37a83849ea1c7a3d94b84542f4faa339013c
nikita-efimov <nikikot@list.ru>
2019-03-18 23:45:25 +0300

    added browser

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.getElementById('user-agent-cont').innerHTML = window.navigator.userAgent
+var user = detect.parse(navigator.userAgent);
+
+  // user.browser.version
+  // user.os.name
+
+document.getElementById('user-agent-cont').innerHTML = user.browser.family + ' ' + user.browser.version + ' ' + user.os.name
